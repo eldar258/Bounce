a6b905d05a21a5d6a127c198353383f947c0edb3
eldar258 <9eldik7@gmail.com>
2021-03-31 21:23:48 +0300

    Класс Info
    Статический класс с общими функциями.
    Добавлена функция для ограничения скорости

---
+++ Assets/Scripts/Info.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+/// <summary>
+/// Класс общих функций
+/// </summary>
+public static class Info
+{
+    /// <summary>
+    /// Позволяет получить вектор силы для Rigidbody.AddForce такой, что бы не превышать заданный вектор скорости
+    /// </summary>
+    /// <param name="curVelocity">Текущая скорость из Rigidbody</param>
+    /// <param name="velocity">Вектор ограничения скорости. Рекомендуется нормализировать вектор,
+    /// а затем скалярно умножить на ограничение - это даст равномерное ограничение во все стороны,
+    /// а не только по осям</param>
+    /// <param name="force">Сила. Чем больше, тем быстрее достигается ограничение, но не может быть больше физических кадров в секунду.
+    /// По умолчанию 1</param>
+    /// <returns>Вектор силы</returns>
+    public static Vector3 forceToReachVelocity(Vector3 curVelocity, Vector3 velocity, float force = 1)
+    {
+        float clampValue = 1 / Time.fixedDeltaTime;
+        force = Mathf.Clamp(force, -clampValue, clampValue);
+        Vector3 result = (velocity.normalized * Vector3.Dot(velocity, curVelocity)) / velocity.magnitude;
+        return (velocity - result) * force;
+    }
+}
