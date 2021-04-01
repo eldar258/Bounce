e1a9a0c409601d2ae87bfc048f047bb07f51cd6e
eldar258 <9eldik7@gmail.com>
2021-04-01 17:56:30 +0300

    Gravipotok update
    Теперь gravitypotok более похож на Excursuon funnel из игры Portal

--- Assets/Scripts/GraviPotok.cs
+++ Assets/Scripts/GraviPotok.cs
@@ -4,26 +4,40 @@ using UnityEngine;
 
 public class GraviPotok : MonoBehaviour
 {
-    public float force = 10;
-    public Transform centerMass;
+    public float force = 3;
+    public float maxSpeed = 3;
 
     private void OnTriggerEnter(Collider other)
     {
         Rigidbody rb = other.attachedRigidbody;
-        rb.constraints = RigidbodyConstraints.FreezePosition;
-        rb.constraints = RigidbodyConstraints.None;
+        rb.velocity = Vector3.zero;
     }
     private void OnTriggerStay(Collider other)
     {
         Rigidbody rb = other.attachedRigidbody;
         rb.useGravity = false;
-        Vector3 vecCenterMass = centerMass.position - rb.position;
-        vecCenterMass.Normalize();
-        other.GetComponent<Rigidbody>().AddForce(vecCenterMass * force);
+
+        Vector3 pointOnVectorZ = calculatePointOnVectorZ(rb);
+        Vector3 vectorPointZ = pointOnVectorZ - rb.position;
+        Vector3 forceToZ = Info.forceToReachVelocity(rb.velocity, vectorPointZ.normalized * maxSpeed, force);
+        rb.AddForce(forceToZ * 3);
+
+        Vector3 forceForward = Info.forceToReachVelocity(rb.velocity, transform.up * maxSpeed, float.MaxValue);
+        rb.AddForce(forceForward);
+
+       
+        //rb.AddForce((vectorPointZ + vectorPotok).normalized * Force);
     }
     private void OnTriggerExit(Collider other)
     {
         Rigidbody rb = other.attachedRigidbody;
         rb.useGravity = true;
+    }
+
+    private Vector3 calculatePointOnVectorZ(Rigidbody rb)
+    {
+        Vector3 pointOnVectorZ = new Vector3(0, transform.InverseTransformPoint(rb.position).y, 0);
+        pointOnVectorZ = transform.TransformPoint(pointOnVectorZ);
+        return pointOnVectorZ;
     }
 }
